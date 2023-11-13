4ec9d25f4d63800b597168a5c5c2c875b77cf81d
MarcCrusellas <71900639+MarcCrusellas@users.noreply.github.com>
2023-11-13 19:42:57 +0100

    tot 2

--- main.ts
+++ main.ts
@@ -1,6 +1,38 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("-")
+    cech(receivedNumber)
+})
+function cech (num: number) {
+    if (randSent != -1) {
+        if (randSent <= num) {
+            basic.showLeds(`
+                . # . # .
+                . . . . .
+                # . . . #
+                . # # # .
+                . . . . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+        }
+    }
+}
 input.onButtonPressed(Button.A, function () {
     radio.sendString("aaaa")
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
+input.onButtonPressed(Button.B, function () {
+    randSent = randint(0, 6)
+    radio.sendNumber(0)
+    basic.showString("" + (randSent))
+})
+let randSent = 0
+randSent = -1
