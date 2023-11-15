0c161addf6daff5bf53535193c2f1cf56bae97f3
MarcCrusellas <71900639+MarcCrusellas@users.noreply.github.com>
2023-11-15 17:45:12 +0100

    Actualització main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,27 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showString("-")
-    cech(receivedNumber)
+    randReceived = receivedNumber
+    if (randSent != -1) {
+        cech(receivedNumber)
+    }
 })
 function cech (num: number) {
-    if (randSent != -1) {
-        if (randSent <= num) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                # . . . #
-                . # # # .
-                . . . . .
-                `)
-        } else {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                . # # # .
-                # . . . #
-                `)
-        }
+    if (randSent <= num) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
     }
 }
 input.onButtonPressed(Button.A, function () {
@@ -30,9 +31,14 @@ radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-    randSent = randint(0, 6)
-    radio.sendNumber(0)
+    randSent = randint(0, 5)
+    radio.sendNumber(randSent)
     basic.showString("" + (randSent))
+    if (randReceived != -1) {
+        cech(randSent)
+    }
 })
+let randReceived = 0
 let randSent = 0
 randSent = -1
+randReceived = -1
